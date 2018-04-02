f0987822bd44b3e8da75f0b7f3757adf70989035
Matt Brennan <matt@153.io>
2018-04-02 10:00:28 +0100

    add setUrl and stop

--- reactive-history.js
+++ reactive-history.js
@@ -2,9 +2,16 @@ import {ReactiveVar} from 'meteor/reactive-var';
 
 const currentUrl = new ReactiveVar(location.pathname);
 
+export const setUrl = url => {
+	currentUrl.set(url);
+};
+
+export const setUrlFromLocation = () => {
+	setUrl(location.pathname);
+};
 
 export const navigate = url => {
-	currentUrl.set(url);
+	setUrl(url);
 	window.history.pushState({}, null, url);
 };
 
@@ -15,9 +22,11 @@ export const link = ev => {
 
 export const start = () => {
 	currentUrl.set(location.pathname);
-	window.addEventListener('popstate', () => {
-		currentUrl.set(location.pathname);
-	});
+	window.addEventListener('popstate', setUrlFromLocation);
+};
+
+export const stop = () => {
+	window.removeEventListener('popstate', setUrlFromLocation);
 };
 
 export const history = currentUrl;
